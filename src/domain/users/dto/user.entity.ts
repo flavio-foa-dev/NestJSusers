11e601fa0926e331ebfa6b6c36aa0f61b6b5848c
Flavio Andrade <foa12381@gmail.com>
2022-12-29 18:26:16 -0300

    validator/userEmailUnique

--- src/domain/users/dto/user.entity.ts
+++ src/domain/users/dto/user.entity.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsUserAlreadyExist } from '../validations/is-name-unique';
+
 export class UserEntity {
   id?: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsUserAlreadyExist({ message: 'name already exists' })
   userName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
